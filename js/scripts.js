92a70a93c57e28eaf3d1e1c311d1926422ee48e9
Chris Swanner and Joel Adams <chris@>
2018-01-16 15:51:10 -0800

    add the whole website

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  var suits = ["Clubs", "Spades", "Diamonds", "Hearts"];
+  var ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
+  var cards = [];
+  $('#button').click(function(){
+      suits.forEach(function(suit) {
+        ranks.forEach(function(rank) {
+        cards.push(rank + " of " + suit);
+      });
+    });
+    cards.forEach(function(card) {
+      $(".result").append("<li>" + card + "</li>");
+    });
+  });
+});
