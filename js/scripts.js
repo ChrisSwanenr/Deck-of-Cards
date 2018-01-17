fb9e70eababbada0e80e5d479bcc942383e33ae2
Chris Swanner and Joel Adams <joel@>
2018-01-16 16:21:21 -0800

    add all seeing eye

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,15 @@ $(document).ready(function() {
   var ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
   var cards = [];
   $('#button').click(function(){
-      suits.forEach(function(suit) {
-        ranks.forEach(function(rank) {
-        cards.push(rank + " of " + suit);
+    $("#initial").text("BIG DECK DETECTED");
+    suits.forEach(function(suit) {
+      ranks.forEach(function(rank) {
+      cards.push(rank + " of " + suit);
       });
     });
     cards.forEach(function(card) {
       $(".result").append("<li>" + card + "</li>");
     });
+
   });
 });
